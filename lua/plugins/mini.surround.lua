be7582e6ede5a85a4490ae0f610cfd3d3088a292
cofeek-codes <kormyshev11@mail.ru>
2024-08-07 04:34:44 +0500

    surrounding binds

---
+++ lua/plugins/mini.surround.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "echasnovski/mini.surround",
+    recommended = true,
+    keys = function(_, keys)
+      -- Populate the keys based on the user's options
+      local opts = LazyVim.opts("mini.surround")
+      local mappings = {
+        { opts.mappings.add, desc = "Add Surrounding", mode = { "n", "v" } },
+        { opts.mappings.delete, desc = "Delete Surrounding" },
+        { opts.mappings.find, desc = "Find Right Surrounding" },
+        { opts.mappings.find_left, desc = "Find Left Surrounding" },
+        { opts.mappings.highlight, desc = "Highlight Surrounding" },
+        { opts.mappings.replace, desc = "Replace Surrounding" },
+        { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+      }
+      mappings = vim.tbl_filter(function(m)
+        return m[1] and #m[1] > 0
+      end, mappings)
+      return vim.list_extend(mappings, keys)
+    end,
+    opts = {
+      mappings = {
+        add = "s", -- Add surrounding in Normal and Visual modes
+        replace = "sr", -- Replace surrounding
+        delete = "sd", -- Delete surrounding
+        -- @WARN: some functions don't work; i'll fix it later
+        find = "sf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "sh", -- Highlight surrounding
+        update_n_lines = "sn", -- Update `n_lines`
+      },
+    },
+  },
+}
