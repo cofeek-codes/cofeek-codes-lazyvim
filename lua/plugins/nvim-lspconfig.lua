95e9129734916e338f8aec3709ed4b5dedeccf9d
cofeek-codes <kormyshev11@mail.ru>
2024-08-09 23:29:19 +0500

    lspconfig messages fomat now looking good

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -3,6 +3,12 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       diagnostics = {
+        signs = {
+          { name = "DiagnosticSignError", text = "E" },
+          { name = "DiagnosticSignWarning", text = "W" },
+          { name = "DiagnosticSignInformation", text = "I" },
+          { name = "DiagnosticSignHint", text = "H" },
+        },
         virtual_text = {
           prefix = "■",
           header = "",
