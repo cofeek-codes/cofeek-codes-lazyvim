7899ce749d67923fade6177f174718cd7e7f2d77
cofeek-codes <kormyshev11@mail.ru>
2024-08-14 23:37:18 +0500

    markdown latex rendering setup

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,11 +1,19 @@
 return {
   {
     "MeanderingProgrammer/markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
     opts = {
       heading = {
         enabled = true,
         sign = false,
         icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+      },
+      latex = {
+        enabled = true,
+        converter = "latex2text",
+        highlight = "RenderMarkdownMath",
+        top_pad = 0,
+        bottom_pad = 0,
       },
       code = {
         style = "language",
