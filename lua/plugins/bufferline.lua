135ed5ea7d65527468e84d98445cec676f485199
cofeek-codes <kormyshev11@mail.ru>
2024-08-22 17:39:04 +0500

    bufferline typos

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,7 +5,7 @@ return {
     keys = {
       { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
       { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-      { "to", "<Cmd>BufferLineCloseOthers<CR>", desc = "Prev tab" }, -- @NOTE: dosen't work; throws error
+      { "to", "<Cmd>BufferLineCloseOthers<CR>", desc = "Close other tabs" }, -- @NOTE: dosen't work; throws error
     },
     opts = {
       options = {
