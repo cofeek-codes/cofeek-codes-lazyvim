f51ade4dcd352d946205a790f9121600a8d3a639
cofeek-codes <kormyshev11@mail.ru>
2024-08-24 23:05:48 +0500

    php formatter fixed

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,6 +2,9 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      formatters_by_ft = {
+        php = { "phpcs", lsp_format = "prefer" },
+      },
       formatters = {
         prettier = {
           prepend_args = { "--no-semi", "--single-quote", "--jsx-single-quote", "--arrow-parens=avoid" },
