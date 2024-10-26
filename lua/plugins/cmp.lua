8e1ec27a3d4fafaccdc6dd7ae535a546dcaf4832
cofeek-codes <kormyshev11@mail.ru>
2024-10-26 21:32:36 +0500

    [cmp.lua]: cmp confirm behaviour changed to 'replace'

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,6 +3,7 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = { "hrsh7th/cmp-emoji" },
     opts = function(_, opts)
+      local cmp = require("cmp")
       table.insert(opts.sources, { name = "emoji" })
       table.insert(opts.sources, { name = "orgmode" })
       table.insert(opts.sources, { name = "buffer" })
@@ -13,6 +14,12 @@ return {
           winblend = 0,
         },
       }
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        }),
+      })
     end,
   },
 }
